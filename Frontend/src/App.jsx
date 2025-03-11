2018bc38c828e0a75828df6a311735e2d1034510
Shubham kahar <Shubhamkahar196@gmail.com>
2025-03-11 12:05:08 +0530

    Adding Proxy

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,12 +21,12 @@ function App() {
         <p>JOKES : {jokes.length}</p>
 
         {
-          jokes.map((joke,index) =>{
+          jokes.map((joke,index) =>(
             <div key={joke.id}>
               <h3>{joke.title}</h3>
               <p>{joke.content}</p>
             </div>
-          })
+          ))
         }
     </>
   )
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {     // adding proxy 
     proxy: {
-      '/api' : 'http://locahost:3000',
+      '/api' : 'http://localhost:3000',
     }
   },
   plugins: [react()],
